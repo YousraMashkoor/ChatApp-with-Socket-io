e3ea18e1145b6ecc7260bb24474ae2bf8ebcc589
unknown <yousra.mashkoor@gmail.com>
2020-10-01 21:42:18 +0500

    recieveing chat

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet, Text, View , TextInput} from "react-native";
+import io from "socket.io-client";
 
 export default function App() {
+  
+  const [messageToSend, setMessageToSend] = useState("");
+  const [recvMessages, setRecvMessages] = useState([]);
+  const socket = useRef(null);
+
+  useEffect(() => {
+    socket.current = io("http://192.168.10.8:3001");
+    socket.current.on("message", message => {
+      setRecvMessages(prevState => [...prevState, message]);
+    });
+  }, []);
+
+  const sendMessage = () => {
+    socket.current.emit("message", messageToSend);
+    setMessageToSend("");
+  };
+
+  const textOfRecvMessages = recvMessages.map(msg => (
+    <Text key={msg}>{msg}</Text>
+  ));
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {textOfRecvMessages}
+      <TextInput
+        value={messageToSend}
+        onChangeText={text => setMessageToSend(text)}
+        placeholder="Enter chat messsage.."
+        onSubmitEditing={sendMessage}
+      />
     </View>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import io from "socket.io-client";
+
+export default function HomeScreen() {
+
+  useEffect(function(){
+    io("http://192.168.10.8:3001")
+  },[])
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
