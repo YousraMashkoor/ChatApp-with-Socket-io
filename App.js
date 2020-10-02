26c3aab0afa205a9f13cb3fbf82d5ff9d150ae62
unknown <yousra.mashkoor@gmail.com>
2020-10-02 16:09:06 +0500

    building ui

--- App.js
+++ App.js
@@ -1,46 +1,46 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, Text, View , TextInput} from "react-native";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
 import io from "socket.io-client";
+import { GiftedChat } from "react-native-gifted-chat";
+import JoinScreen from "./screens/JoinScreen";
 
-export default function App() {
-  
-  const [messageToSend, setMessageToSend] = useState("");
+export default function HomeScreen() {
   const [recvMessages, setRecvMessages] = useState([]);
+  const [hasJoined, setHasJoined] = useState(false);
   const socket = useRef(null);
 
   useEffect(() => {
-    socket.current = io("http://192.168.10.8:3001");
+    socket.current = io("http://192.168.10.5:3001");
     socket.current.on("message", message => {
-      setRecvMessages(prevState => [...prevState, message]);
+      setRecvMessages(prevState => GiftedChat.append(prevState, message));
     });
   }, []);
 
-  const sendMessage = () => {
-    socket.current.emit("message", messageToSend);
-    setMessageToSend("");
+  const onSend = messages => {
+    socket.current.emit("message", messages[0].text);
+    setRecvMessages(prevState => GiftedChat.append(prevState, messages));
   };
 
-  const textOfRecvMessages = recvMessages.map(msg => (
-    <Text key={msg}>{msg}</Text>
-  ));
+  const joinChat = username => {
+    socket.current.emit("join", username);
+    setHasJoined(true);
+  };
+
   return (
-    <View style={styles.container}>
-      {textOfRecvMessages}
-      <TextInput
-        value={messageToSend}
-        onChangeText={text => setMessageToSend(text)}
-        placeholder="Enter chat messsage.."
-        onSubmitEditing={sendMessage}
-      />
+    <View style={{ flex: 1 }}>
+      {hasJoined ? (
+        <GiftedChat
+          renderUsernameOnMessage
+          messages={recvMessages}
+          onSend={messages => onSend(messages)}
+          user={{
+            _id: 1
+          }}
+        />
+      ) : (
+        <JoinScreen joinChat={joinChat} />
+      )}
+      {Platform.OS === "android" && <KeyboardAvoidingView behavior="padding" />}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,10 +1,23 @@
 const io = require("socket.io")();
+const messageHandler = require("./handlers/message.handler");
+
+let currentUserId = 2;
+const users = {};
+
+function createUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
 
 io.on("connection", socket => {
   console.log("a user connected!");
-  socket.on("message", message => {
-    console.log(message);
-    io.emit("message", message);
+  console.log(socket.id);
+  users[socket.id] = { userId: currentUserId++ };
+  socket.on("join", username => {
+    users[socket.id].username = username;
+    users[socket.id].avatar = createUserAvatarUrl();
+    messageHandler.handleMessage(socket, users);
   });
 });
 
